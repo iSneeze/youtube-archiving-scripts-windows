b3038e339993536030dbc21e4acdea4cc7f76da9
iSneeze <justopenanissue@github>
2023-10-06 14:52:54 +0200

    updated update script

--- update.bat
+++ update.bat
@@ -2,8 +2,8 @@
 pushd "%~dp0\binaries"
 del *.exe
 mkdir temp
-curl -L https://github.com/Kethsar/ytarchive/releases/download/latest/ytarchive_windows_amd64.zip > .\temp\ytarchive.zip
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.07.06/yt-dlp.exe > yt-dlp.exe
+curl -L https://github.com/Kethsar/ytarchive/releases/latest/download/ytarchive_windows_amd64.zip > .\temp\ytarchive.zip
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe > yt-dlp.exe
 
 tar -xvzf ".\temp\ytarchive.zip"
 rmdir /Q /S "temp"
